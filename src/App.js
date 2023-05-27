e3138b8ced244992d5ded5c0278b23dc9dd32317
Daniel FCS <danielf.cs@outlook.com>
2023-05-27 11:49:19 +0100

    first commit-base done no api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
+
+    //listing
+  const [allItems,setAllItems] = useState('')
+  const [filteredItems,setFilteredItems] = useState('')
+  const [categoryList,setCategoryList] = useState('')
+
+    //selection
+
+  const [selectedItem,setSeletecItem] = useState('')
+
+  const handleFilter = () => {
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route/>
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
